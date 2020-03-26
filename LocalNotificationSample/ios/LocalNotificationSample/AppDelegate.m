484a94cdb3fdcefb6bd55c68893ef63aa0f5eec1
Alan Wang <awangvan@gmail.com>
2020-03-26 09:13:49 -0700

    MOBILSDK-2795: Fixed a bug where tapping a local notification not opening a survey

--- LocalNotificationSample/ios/LocalNotificationSample/AppDelegate.m
+++ LocalNotificationSample/ios/LocalNotificationSample/AppDelegate.m
@@ -10,11 +10,15 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <ForeSee/ForeSee.h>
+
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"LocalNotificationSample"
@@ -37,6 +41,27 @@
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
+}
+
+#pragma mark - UNUserNotificationCenterDelegate
+
+// Called when a notification is delivered to the foregrounded app
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+       willPresentNotification:(UNNotification *)notification
+         withCompletionHandler:(nonnull void (^)(UNNotificationPresentationOptions))completionHandler
+{
+    NSLog(@"User Info : %@",notification.request.content.userInfo);
+    completionHandler(UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionBadge);
+}
+
+// Called to let your app know which action was selected by the user for a given notification
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+didReceiveNotificationResponse:(UNNotificationResponse *)response
+         withCompletionHandler:(nonnull void (^)(void))completionHandler
+{
+    [ForeSee showSurveyForNotificationResponse:response];
+    NSLog(@"User Info : %@",response.notification.request.content.userInfo);
+    completionHandler();
 }
 
 @end
